a530013ecb1f30a03d04dc9de2d5ad3715a3a81d
Marat Timofeev <marat.tmv1@gmail.com>
2026-02-26 13:27:57 +0700

    Добавил сортировку импортов, донастроил линтер

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
+const simpleImportSort = require("eslint-plugin-simple-import-sort");
+const unusedImports = require("eslint-plugin-unused-imports");
 
 module.exports = defineConfig([
   expoConfig,
@@ -8,9 +10,18 @@ module.exports = defineConfig([
   },
   {
     files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
     rules: {
       semi: ["error", "never"],
+      "comma-spacing": ["error", { before: false, after: true }],
+      "object-curly-spacing": ["error", "always"],
       "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
+      "unused-imports/no-unused-imports": "warn",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
