e854dbab49326e16718d479da690f97994556246
Marat Timofeev <marat.tmv1@gmail.com>
2026-03-16 21:09:28 +0700

    Убрал мусор

--- mobile/constants/config.ts
+++ mobile/constants/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
-  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL || "https://api.subtrack.study",
+  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
   google: {
-    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID || "",
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
   },
 } as const
