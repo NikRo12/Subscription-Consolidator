58fb0bed6eba0f7b61453dee79c586c53662f1c4
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2026-03-17 01:10:05 +0700

    Исправлены недочёты и дополнены возможности Redis Client

--- Backend/internal/services/redis/access.go
+++ Backend/internal/services/redis/access.go
@@ -2,9 +2,6 @@ package redis
 
 import (
 	"os"
-
-	"github.com/NikRo12/Subscription-Consolidator/Backend/internal/services/commonerrors"
-	"github.com/joho/godotenv"
 )
 
 /*
@@ -13,9 +10,6 @@ It tries to get the redis' addr
 If there're no REDIS_ADDR variable, it returns `localhost:6379`
 */
 func GetRedisAddr() (string, error) {
-	if err := godotenv.Load(); err != nil {
-		return "", commonerrors.NoENVFile
-	}
 	addr := os.Getenv("REDIS_ADDR")
 	if addr == "" {
 		addr = "localhost:6379"
--- Backend/internal/services/redis/interaction.go
+++ Backend/internal/services/redis/interaction.go
@@ -23,25 +23,36 @@ func NewRedisConn(client *redis.Client) *RedisConn {
 /*
 Closing connection
 */
-func (conn *RedisConn) terminate() error {
+func (conn *RedisConn) Terminate() error {
+
 	if err := conn.client.Close(); err != nil {
-		return fmt.Errorf("[ERROR]! Redis-client: %v", err)
+		return fmt.Errorf("[ERROR]! redis-client: %w", err)
 	}
+
 	return nil
 }
 
 /*
 Push the task to a redis list
 */
-func (conn *RedisConn) PushTask(ctx context.Context, task *models.Task) error {
+func (conn *RedisConn) PushTask(
+	ctx context.Context,
+	task *models.Task) error {
+
 	byteData, err := json.Marshal(task)
+
 	if err != nil {
-		return fmt.Errorf("[ERROR]! Json-marhslling: %v", err)
+		return fmt.Errorf("[ERROR]! json-marhslling: %w", err)
 	}
-	err = conn.client.LPush(ctx, models.TaskQueueName, byteData).Err()
+
+	err = conn.client.LPush(ctx,
+		models.TaskQueueName,
+		byteData).Err()
+
 	if err != nil {
-		return fmt.Errorf("[ERROR]! Redis-lpush: %v", err)
+		return fmt.Errorf("[ERROR]! redis-lpush: %w", err)
 	}
+
 	return nil
 }
 
@@ -49,16 +60,74 @@ func (conn *RedisConn) PushTask(ctx context.Context, task *models.Task) error {
 Blocking operation!
 It waits for any tasks in the queue and takes it
 */
-func (conn *RedisConn) GetTask(ctx context.Context) (*models.Task, error) {
+func (conn *RedisConn) GetTask(ctx context.Context) (
+	*models.Task,
+	error) {
 	task := &models.Task{}
+
 	res, err := conn.client.BRPop(ctx, 0, models.TaskQueueName).Result()
+
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR]! Redis-brpop: %v", err)
+		return nil, fmt.Errorf("[ERROR]! redis-brpop: %w", err)
 	}
+
+	if len(res) != 2 {
+		return nil, fmt.Errorf("unexpected redis-format")
+	}
+
 	byteData := []byte(res[1])
+
 	err = json.Unmarshal(byteData, task)
+
 	if err != nil {
-		return nil, fmt.Errorf("[ERROR]! Json-marhslling: %v", err)
+		return nil, fmt.Errorf("[ERROR]! json-marhslling: %w", err)
 	}
+
 	return task, nil
+}
+
+func (conn *RedisConn) PushParseResult(
+	ctx context.Context,
+	parseRes *models.ParseResult) error {
+
+	serializedData, err := json.Marshal(parseRes)
+	if err != nil {
+		return fmt.Errorf("[ERROR]! json marshalling: %w", err)
+	}
+
+	err = conn.client.LPush(ctx,
+		models.ResultQueueName,
+		serializedData).Err()
+
+	if err != nil {
+		return fmt.Errorf("[ERROR]! сannot push the result: %w", err)
+	}
+
+	return nil
+}
+
+func (conn *RedisConn) GetParseResult(ctx context.Context) (
+	*models.ParseResult,
+	error) {
+	parseRes := &models.ParseResult{}
+
+	redisData, err := conn.client.BRPop(ctx, 0, models.ResultQueueName).Result()
+
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR]! сannot read result: %w", err)
+	}
+
+	if len(redisData) != 2 {
+		return nil, fmt.Errorf("unexpected redis-format")
+	}
+
+	serializedRes := []byte(redisData[1])
+
+	err = json.Unmarshal(serializedRes, parseRes)
+
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR]! сannot unmarshal result: %w", err)
+	}
+
+	return parseRes, nil
 }
