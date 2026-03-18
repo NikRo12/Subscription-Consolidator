fd68a20f3d0289da8aaf1cc45569481708b330ef
hugginlu <nr.081105@gmail.com>
2026-03-18 23:21:50 +0700

    Подготовка к деплою, создание Dockerfile

--- Backend/cmd/server/main.go
+++ Backend/cmd/server/main.go
@@ -6,14 +6,7 @@ import (
 
 	"github.com/NikRo12/Subscription-Consolidator/Backend/configs"
 	httpserver "github.com/NikRo12/Subscription-Consolidator/Backend/internal/transport/httpserver"
-	"github.com/joho/godotenv"
 )
-
-func init() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 	flag.Parse()
