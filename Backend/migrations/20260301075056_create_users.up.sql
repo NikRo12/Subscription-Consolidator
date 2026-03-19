759b6b03a723a34ce604a19ffc9dff225a95d8db
hugginlu <nr.081105@gmail.com>
2026-03-19 14:11:29 +0700

    A

--- Backend/migrations/20260301075056_create_users.up.sql
+++ Backend/migrations/20260301075056_create_users.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id            SERIAL PRIMARY KEY,
-    google_id     VARCHAR(255) UNIQUE NOT NULL,
-    refresh_token VARCHAR(255) NOT NULL,
-    access_token  VARCHAR(255) NOT NULL
+    google_id     VARCHAR(1025) UNIQUE NOT NULL,
+    refresh_token VARCHAR(1025) NOT NULL,
+    access_token  VARCHAR(1025) NOT NULL
 );
