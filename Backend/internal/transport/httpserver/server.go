47a0dd56e156c7e356677a8b5a5f5a5f92e56bdd
hugginlu <nr.081105@gmail.com>
2026-03-20 09:18:26 +0700

    CORS

--- Backend/internal/transport/httpserver/server.go
+++ Backend/internal/transport/httpserver/server.go
@@ -206,6 +206,14 @@ func (s *server) authenticateUser(next http.HandlerFunc) http.HandlerFunc {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
